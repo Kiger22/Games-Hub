fab9e97bd42fe13369f866dec33c8108743ac4d3
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-08-14 02:25:42 +0200

    Create hero.js

---
+++ KGRGames/src/pages/hero/hero.js
@@ -0,0 +1,42 @@
+import("./hero.css");
+import { divApp } from "../../../main"
+import { GAMES } from "../../data/games";
+
+export const createHero = () => {
+  divApp.innerHTML = "";
+  heroPage();
+}
+
+export const heroPage = () => {
+
+  //Seccion
+  const heroSection = document.createElement("section");
+  heroSection.className = "heroSection";
+  divApp.appendChild(heroSection);
+
+  //Titulo de seccion
+  const sectionTitlle = document.createElement("h1");
+  sectionTitlle.classList = "section-tittle";
+  sectionTitlle.innerText = "Game Hub";
+  heroSection.appendChild(sectionTitlle);
+
+  //Cuerpo de la seccion
+  const bodySection = document.createElement("div");
+  bodySection.className = "bodySectionHero";
+  bodySection.id = "body-section-hero";
+  heroSection.appendChild(bodySection);
+
+  //Cartas de juegos
+  GAMES.forEach(game => {
+    const gameCard = document.createElement("div");
+    gameCard.classList.add("game-card");
+    gameCard.innerHTML = `
+        <img src="${game.imageUrl}" alt="${game.title}">
+        <h3>${game.title}</h3>
+        <p>${game.description}</p>
+    `;
+    gameCard.addEventListener('click', game.openPage);
+    bodySection.appendChild(gameCard);
+  })
+
+}
