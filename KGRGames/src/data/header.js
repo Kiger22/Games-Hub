9deca110ec1218d015408f15a515c16d0b784442
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-08-14 02:25:29 +0200

    Create header.js

---
+++ KGRGames/src/data/header.js
@@ -0,0 +1,18 @@
+import { createHero } from "../pages/hero/hero";
+
+export const meniItemsMenuToggle = [
+  { href: '#', imgSrc: "./assets/menu.svg", alt: 'Menu' }
+];
+
+export const menuItemsHeaderI = [
+  {
+    icon: "./assets/icons8-casa.svg", text: "", href: createHero,
+    icon: "./assets/icons8-casa.svg", text: "", href: createHero
+  },
+];
+
+export const menuItemsHeaderII = [
+
+];
+
+export const heaterLogo = "./assets/logo-solo-claro.png"
