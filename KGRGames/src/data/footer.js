c04991a6a87a684e72f14d1551f440243744756b
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-08-14 02:25:23 +0200

    Create footer.js

---
+++ KGRGames/src/data/footer.js
@@ -0,0 +1,11 @@
+export const footerLogo = "./assets/logo-nombre-claro.png"
+
+export const menuFooter = [
+  { text: 'Inicio', href: '#' },
+];
+
+export const socialLinks = [
+  { href: "https://github.com/", src: "./assets/github.png", },
+  { href: "https://www.linkedin.com/", src: "./assets/linkedin.png" },
+  { href: "https://discord.com/", src: "./assets/discord.png" }
+];
